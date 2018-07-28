d1736a207127f7c8dc429ea4d8c8193d785a62c6
Aaron Harburg <connect@aaronharburg.com>
2018-07-27 20:32:48 -0400

    prod.js update

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  mongodb://aharburg-admin:.Do(BQ6rA8yp]2opshJLz6b9pti9jDy6(yzuwZow86b#MZXJDhzuvHp.BX*?G9($@ds151530.mlab.com:51530/emaily-prod
+
 };
-mongodb://aharburg-admin:.Do(BQ6rA8yp]2opshJLz6b9pti9jDy6(yzuwZow86b#MZXJDhzuvHp.BX*?G9($@ds151530.mlab.com:51530/emaily-prod
